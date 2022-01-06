858849758706c9208c804ad8d6ceeb6736fa18c6
Xyphuz <hansen0715.2@gmail.com>
2022-01-06 19:54:39 +0800

    feat: implement csharp writer

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ release-windows:
 release-linux:
 	@REM avoid white space after env variable
 	@set CGO_ENABLED=0&& set GOOS=linux&& set GOARCH=amd64&& go build -o .\convjson
+
+release-all:
+	@make release-windows
+	@make release-linux
---
+++ model/csharp_writer.go
@@ -0,0 +1,81 @@
+package model
+
+import "os"
+
+type CSharpWriter struct {
+	outputPath string
+}
+
+func (csw *CSharpWriter) SetOutputPath(outputPath string) {
+	csw.outputPath = outputPath
+}
+
+func (csw *CSharpWriter) SetNested(nested bool) {}
+
+func (csw *CSharpWriter) Write(abstractStructs []Struct) error {
+	file, err := os.Create(csw.outputPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for _, abstractStruct := range abstractStructs {
+		if abstractStruct.Name == "Nested" {
+			continue
+		}
+
+		_, err := file.WriteString(csw.GetStruct(abstractStruct))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (csw *CSharpWriter) GetStruct(abstractStruct Struct) string {
+	return "public class " + abstractStruct.Name + " {\n" + csw.GetFields(abstractStruct.Fields) + "}\n\n"
+}
+
+func (csw *CSharpWriter) GetFields(fields []Field) string {
+	var result string = ""
+
+	for i, field := range fields {
+		result += csw.GetField(i, field)
+	}
+
+	return result
+}
+
+func (csw *CSharpWriter) GetField(_ int, field Field) string {
+	var typeName string = ""
+
+	if field.TypeName[len(field.TypeName)-2:] == "[]" {
+		typeName = "List<" + csw.GetTypeName(field.TypeName[:len(field.TypeName)-2]) + ">"
+	} else {
+		typeName = csw.GetTypeName(field.TypeName)
+	}
+
+	return "\tpublic " + typeName + " " + field.Index + ";\n"
+}
+
+func (csw *CSharpWriter) GetTypeName(typeName string) string {
+	switch typeName {
+	case "int":
+		return "int"
+	case "int32":
+		return "int"
+	case "int64":
+		return "long"
+	case "float32":
+		return "float"
+	case "float64":
+		return "double"
+	case "string":
+		return "String"
+	case "bool":
+		return "boolean"
+	default:
+		return typeName
+	}
+}
